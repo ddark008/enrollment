33b178622bd959c32625b5cef8026e42393d5935
Дмитрий А. Елисеев <eliseev_d@ntcees.ru>
2022-09-09 08:39:17 +0300

    Init spring boot

---
+++ src/main/java/ru/ddark008/yadisk/YadiskApplication.java
@@ -0,0 +1,13 @@
+package ru.ddark008.yadisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YadiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YadiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ddark008/yadisk/YadiskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddark008.yadisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YadiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
